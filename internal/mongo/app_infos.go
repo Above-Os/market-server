5ed297cceeeef51d5af90fa5602503ca878b8818
Sai <kldtks@live.com>
2024-12-20 14:43:53 +0800

    Attempt to fix the loss of old version information when updating the version.

--- internal/mongo/app_infos.go
+++ internal/mongo/app_infos.go
@@ -154,8 +154,23 @@ func DisableAppInfoToDb(appInfo *models.ApplicationInfoFullData) error {
 func UpsertAppInfoToDb(appInfo *models.ApplicationInfoFullData) error {
 	filter := bson.M{"name": appInfo.Name}
 	updatedDocument := &models.ApplicationInfoFullData{}
-	update := getUpdates(appInfo)
-	u := bson.M{"$set": update}
+	updateApp := getUpdatesAppinfo(appInfo)
+	updateVersion := getUpdatesVersion(appInfo)
+	updateLatest := getUpdatesLatest(appInfo)
+
+	// u := bson.M{"$set": update}
+	u := bson.M{
+		"set": updateApp,
+		"$set": bson.M{
+			"history.latest": updateLatest,
+		},
+		"$push": bson.M{
+			"history": bson.M{
+				"$each":     []interface{}{updateVersion},
+				"$position": 0,
+			},
+		},
+	}
 	opts := options.FindOneAndUpdate().SetUpsert(true)
 
 	err := mgoClient.findOneAndUpdate(AppStoreDb, AppInfosCollection, filter, u, opts).Decode(updatedDocument)
@@ -169,11 +184,20 @@ func UpsertAppInfoToDb(appInfo *models.ApplicationInfoFullData) error {
 	return err
 }
 
-func getUpdates(appInfoNew *models.ApplicationInfoFullData) *bson.M {
-
+func getUpdatesAppinfo(appInfoNew *models.ApplicationInfoFullData) *bson.M {
 	update := bson.M{}
+
+	nameMd58 := utils.Md5String(appInfoNew.Name)[:8]
+
+	update["id"] = nameMd58
+	update["appLabels"] = appInfoNew.History["latest"].AppLabels
+
+	return &update
+}
+
+func getUpdatesLatest(appInfoNew *models.ApplicationInfoFullData) *bson.M {
+
 	latest := bson.M{}
-	version := bson.M{}
 
 	latest["name"] = appInfoNew.History["latest"].Name
 	latest["lastCommitHash"] = appInfoNew.History["latest"].LastCommitHash
@@ -225,6 +249,15 @@ func getUpdates(appInfoNew *models.ApplicationInfoFullData) *bson.M {
 	latest["namespace"] = appInfoNew.History["latest"].Namespace
 	latest["onlyAdmin"] = appInfoNew.History["latest"].OnlyAdmin
 
+	return &latest
+}
+
+func getUpdatesVersion(appInfoNew *models.ApplicationInfoFullData) *bson.M {
+
+	version := bson.M{}
+
+	nameMd58 := utils.Md5String(appInfoNew.Name)[:8]
+
 	// version
 	version["name"] = appInfoNew.History["latest"].Name
 	version["lastCommitHash"] = appInfoNew.History["latest"].LastCommitHash
@@ -269,19 +302,130 @@ func getUpdates(appInfoNew *models.ApplicationInfoFullData) *bson.M {
 	version["sourceCode"] = appInfoNew.History["latest"].SourceCode
 	version["license"] = appInfoNew.History["latest"].License
 	version["legal"] = appInfoNew.History["latest"].Legal
-	//update["status"] = appInfoNew.Status
+
 	version["appLabels"] = appInfoNew.History["latest"].AppLabels
 	version["modelSize"] = appInfoNew.History["latest"].ModelSize
 	version["namespace"] = appInfoNew.History["latest"].Namespace
 	version["onlyAdmin"] = appInfoNew.History["latest"].OnlyAdmin
 
-	update["history"] = bson.M{
-		"latest":                             latest,
-		appInfoNew.History["latest"].Version: version,
-	}
-
-	update["id"] = nameMd58
-	update["appLabels"] = appInfoNew.History["latest"].AppLabels
-
-	return &update
+	return &version
 }
+
+// func getUpdates(appInfoNew *models.ApplicationInfoFullData) *bson.M {
+
+// 	update := bson.M{}
+// 	latest := bson.M{}
+// 	version := bson.M{}
+
+// 	latest["name"] = appInfoNew.History["latest"].Name
+// 	latest["lastCommitHash"] = appInfoNew.History["latest"].LastCommitHash
+// 	latest["updateTime"] = appInfoNew.History["latest"].UpdateTime
+// 	latest["createTime"] = appInfoNew.History["latest"].CreateTime
+
+// 	latest["chartName"] = appInfoNew.History["latest"].ChartName
+// 	latest["cfgType"] = appInfoNew.History["latest"].CfgType
+// 	latest["icon"] = appInfoNew.History["latest"].Icon
+// 	latest["desc"] = appInfoNew.History["latest"].Description
+// 	nameMd58 := utils.Md5String(appInfoNew.Name)[:8]
+// 	latest["appid"] = nameMd58
+// 	latest["id"] = nameMd58
+// 	latest["title"] = appInfoNew.History["latest"].Title
+// 	latest["version"] = appInfoNew.History["latest"].Version
+// 	latest["categories"] = appInfoNew.History["latest"].Categories
+// 	latest["versionName"] = appInfoNew.History["latest"].VersionName
+// 	latest["fullDescription"] = appInfoNew.History["latest"].FullDescription
+// 	latest["upgradeDescription"] = appInfoNew.History["latest"].UpgradeDescription
+// 	latest["promoteImage"] = appInfoNew.History["latest"].PromoteImage
+// 	latest["promoteVideo"] = appInfoNew.History["latest"].PromoteVideo
+// 	latest["subCategory"] = appInfoNew.History["latest"].SubCategory
+// 	latest["developer"] = appInfoNew.History["latest"].Developer
+// 	latest["requiredMemory"] = appInfoNew.History["latest"].RequiredMemory
+// 	latest["requiredDisk"] = appInfoNew.History["latest"].RequiredDisk
+// 	latest["supportClient"] = appInfoNew.History["latest"].SupportClient
+// 	latest["supportArch"] = appInfoNew.History["latest"].SupportArch
+// 	latest["requiredGpu"] = appInfoNew.History["latest"].RequiredGPU
+// 	latest["requiredCpu"] = appInfoNew.History["latest"].RequiredCPU
+// 	latest["rating"] = appInfoNew.History["latest"].Rating
+// 	latest["target"] = appInfoNew.History["latest"].Target
+// 	latest["permission"] = appInfoNew.History["latest"].Permission
+// 	latest["entrances"] = appInfoNew.History["latest"].Entrances
+// 	latest["middleware"] = appInfoNew.History["latest"].Middleware
+// 	latest["options"] = appInfoNew.History["latest"].Options
+// 	latest["locale"] = appInfoNew.History["latest"].Locale
+// 	latest["i18n"] = appInfoNew.History["latest"].I18n
+
+// 	latest["submitter"] = appInfoNew.History["latest"].Submitter
+// 	latest["doc"] = appInfoNew.History["latest"].Doc
+// 	latest["website"] = appInfoNew.History["latest"].Website
+// 	latest["featuredImage"] = appInfoNew.History["latest"].FeaturedImage
+// 	latest["sourceCode"] = appInfoNew.History["latest"].SourceCode
+// 	latest["license"] = appInfoNew.History["latest"].License
+// 	latest["legal"] = appInfoNew.History["latest"].Legal
+// 	//update["status"] = appInfoNew.Status
+// 	latest["appLabels"] = appInfoNew.History["latest"].AppLabels
+// 	latest["modelSize"] = appInfoNew.History["latest"].ModelSize
+// 	latest["namespace"] = appInfoNew.History["latest"].Namespace
+// 	latest["onlyAdmin"] = appInfoNew.History["latest"].OnlyAdmin
+
+// 	// version
+// 	version["name"] = appInfoNew.History["latest"].Name
+// 	version["lastCommitHash"] = appInfoNew.History["latest"].LastCommitHash
+// 	version["updateTime"] = appInfoNew.History["latest"].UpdateTime
+// 	version["createTime"] = appInfoNew.History["latest"].CreateTime
+
+// 	version["chartName"] = appInfoNew.History["latest"].ChartName
+// 	version["cfgType"] = appInfoNew.History["latest"].CfgType
+// 	version["icon"] = appInfoNew.History["latest"].Icon
+// 	version["desc"] = appInfoNew.History["latest"].Description
+// 	version["appid"] = nameMd58
+// 	version["id"] = nameMd58
+// 	version["title"] = appInfoNew.History["latest"].Title
+// 	version["version"] = appInfoNew.History["latest"].Version
+// 	version["categories"] = appInfoNew.History["latest"].Categories
+// 	version["versionName"] = appInfoNew.History["latest"].VersionName
+// 	version["fullDescription"] = appInfoNew.History["latest"].FullDescription
+// 	version["upgradeDescription"] = appInfoNew.History["latest"].UpgradeDescription
+// 	version["promoteImage"] = appInfoNew.History["latest"].PromoteImage
+// 	version["promoteVideo"] = appInfoNew.History["latest"].PromoteVideo
+// 	version["subCategory"] = appInfoNew.History["latest"].SubCategory
+// 	version["developer"] = appInfoNew.History["latest"].Developer
+// 	version["requiredMemory"] = appInfoNew.History["latest"].RequiredMemory
+// 	version["requiredDisk"] = appInfoNew.History["latest"].RequiredDisk
+// 	version["supportClient"] = appInfoNew.History["latest"].SupportClient
+// 	version["supportArch"] = appInfoNew.History["latest"].SupportArch
+// 	version["requiredGpu"] = appInfoNew.History["latest"].RequiredGPU
+// 	version["requiredCpu"] = appInfoNew.History["latest"].RequiredCPU
+// 	version["rating"] = appInfoNew.History["latest"].Rating
+// 	version["target"] = appInfoNew.History["latest"].Target
+// 	version["permission"] = appInfoNew.History["latest"].Permission
+// 	version["entrances"] = appInfoNew.History["latest"].Entrances
+// 	version["middleware"] = appInfoNew.History["latest"].Middleware
+// 	version["options"] = appInfoNew.History["latest"].Options
+// 	version["locale"] = appInfoNew.History["latest"].Locale
+// 	version["i18n"] = appInfoNew.History["latest"].I18n
+
+// 	version["submitter"] = appInfoNew.History["latest"].Submitter
+// 	version["doc"] = appInfoNew.History["latest"].Doc
+// 	version["website"] = appInfoNew.History["latest"].Website
+// 	version["featuredImage"] = appInfoNew.History["latest"].FeaturedImage
+// 	version["sourceCode"] = appInfoNew.History["latest"].SourceCode
+// 	version["license"] = appInfoNew.History["latest"].License
+// 	version["legal"] = appInfoNew.History["latest"].Legal
+// 	//update["status"] = appInfoNew.Status
+// 	version["appLabels"] = appInfoNew.History["latest"].AppLabels
+// 	version["modelSize"] = appInfoNew.History["latest"].ModelSize
+// 	version["namespace"] = appInfoNew.History["latest"].Namespace
+// 	version["onlyAdmin"] = appInfoNew.History["latest"].OnlyAdmin
+
+// 	// update["history"] = bson.M{
+// 	// 	"latest":                             latest,
+// 	// 	appInfoNew.History["latest"].Version: version,
+// 	// }
+// 	update["latest"] = latest
+// 	update["version"] = version
+
+// 	update["id"] = nameMd58
+// 	update["appLabels"] = appInfoNew.History["latest"].AppLabels
+
+// 	return &update
+// }
